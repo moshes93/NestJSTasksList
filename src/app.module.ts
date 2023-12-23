ebfcdeba22458dbefa0d2923d8a786c0ff315bba
= <=>
2023-12-23 16:01:26 +0200

    init module with db data

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { APP_FILTER } from '@nestjs/core';
 import { NotFoundFilter } from './filters/NotFoundFilter';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    TasksModule
+  ],
   providers: [
     {
       provide: APP_FILTER,
