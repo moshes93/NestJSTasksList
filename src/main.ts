593000cfaea15cb6a83ee4e1219a8e7728b79780
= <=>
2023-12-23 16:01:49 +0200

    fill table with data on load

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { TasksService } from './tasks/tasks.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,11 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  // Create init data in db onload
+  const tasksService = app.get(TasksService);
+  await tasksService.createTask('Multiply Array', '')
+  await tasksService.createTask('Sum Array', '')
 
   await app.listen(3000);
 }
