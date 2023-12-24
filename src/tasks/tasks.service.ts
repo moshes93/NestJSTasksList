d370fd31b5c18626bab4c9abdd205064f91a4f45
= <=>
2023-12-24 18:42:37 +0200

    remove init testing

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Task } from "./tasks.entity";
 
 @Injectable({})
 export class TasksService {
+    constructor(@InjectRepository(Task) private taskRepository: Repository<Task>) {}
     getAvaiableTasks() {
-        return ['task1', 'task2', 'task3'];
+        return this.taskRepository.find();
     }
 
-    getTaskStatusById(taskId: string) {
-        return `Getting status for task ${taskId}`;
+    createTask(name: string, parameters: string) {
+        const task = new Task();
+        task.name = name;
+        task.parameters = parameters;
+        return this.taskRepository.save(task);
+    }
+
+    async getTaskStatusById(taskId: number) {
+        const task = await this.taskRepository.findOne({where: {id: taskId}})
+        return task;
     }
 
     getTaskResultById(taskId: string) {
