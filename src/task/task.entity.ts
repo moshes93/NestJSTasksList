7377d9229653b3aae2150ffd22112d9bba958ee1
= <=>
2023-12-25 18:41:57 +0200

    one to many connection

--- src/task/task.entity.ts
+++ src/task/task.entity.ts
@@ -1,7 +1,11 @@
-import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { Exec } from "src/exec/exce.entity";
+import { Column, Entity, OneToMany, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Task {
     @PrimaryColumn()
     name: string;
+
+    @OneToMany(() => Exec, (exec) => exec.task)
+    exec: Exec[]
 }
