8087d274c3a274c32817e3937f35de1f71082ec3
= <=>
2023-12-25 22:06:03 +0200

    execution task arbitrator

--- src/exec/utils.ts
+++ src/exec/utils.ts
@@ -6,3 +6,20 @@ export class TaskRequest {
     @ApiProperty()
     parameters: any;
 }
+
+export function handleExecution(taskName: string, parameters: any) {
+    let result;
+    switch(taskName) {
+        case 'addition':
+            const sumArr = parameters as [];
+            result = sumArr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+            break;
+        case 'multiplication':
+            const multArr = parameters as [];
+            result = multArr.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
+            break;
+        default:
+            result = null;
+    }
+    return result;
+}
