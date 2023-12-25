dae90e36bf7db239e688017b1b010a69b2477f1e
= <=>
2023-12-26 00:17:46 +0200

    wrap db change action with try catch and throw error if needed

--- src/exec/exec.service.ts
+++ src/exec/exec.service.ts
@@ -3,26 +3,36 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Exec } from "./exce.entity";
 import { Task } from "src/task/task.entity";
+import { DatabaseActionError } from "src/exceptions/CreateExecException";
 
 @Injectable({})
 export class ExecService {
     constructor(@InjectRepository(Exec) private execRepository: Repository<Exec>) {}
 
-    executeTask(name: string, parameters: object) {
-        const exec = new Exec();
-        const task = new Task();
-        task.name = name;
-        exec.parameters = parameters;
-        exec.task = task;
-        return this.execRepository.save(exec);
+    async executeTask(name: string, parameters: object) {
+        try {
+            const exec = new Exec();
+            const task = new Task();
+            task.name = name;
+            exec.parameters = parameters;
+            exec.task = task;
+            return await this.execRepository.save(exec);
+        }
+        catch (e) {
+            throw new DatabaseActionError('Error on creating new execution task in database', e);
+        }
     }
 
     async updateCompletedTask(taskId: number, result: string) {
-        const exec = await this.getExec(taskId);
-        if (exec) {
-            exec.result = result;
-            exec.status = "completed";
-            return this.execRepository.save(exec);
+        try {
+            const exec = await this.getExec(taskId);
+            if (exec) {
+                exec.result = result;
+                exec.status = "completed";
+                return this.execRepository.save(exec);
+            }
+        } catch (e) {
+            throw new DatabaseActionError('Error on updating execution task in database', e);
         }
     }
 
